307ac439ec2bc0f4806113686d15bae4c3244d86
Marcio Ferreira <analistamarcio@hotmail.com>
2019-10-27 19:33:42 -0700

    Session Authentication

--- src/app/controllers/SessionController.js
+++ src/app/controllers/SessionController.js
@@ -1,22 +1,10 @@
 import jwt from 'jsonwebtoken';
-import * as Yup from 'yup';
 
 import User from '../models/User';
 import authConfig from '../../config/auth';
 
 class SessionController {
   async store(req, res) {
-    const schema = Yup.object().shape({
-      email: Yup.string()
-        .email()
-        .required(),
-      password: Yup.string().required(),
-    });
-
-    if (!(await schema.isValid(req.body))) {
-      return res.status(400).json({ error: 'Validation fails.' });
-    }
-
     const { email, password } = req.body;
 
     const user = await User.findOne({ where: { email } });
@@ -26,7 +14,7 @@ class SessionController {
     }
 
     if (!(await user.checkPassword(password))) {
-      return res.status(401).json({ error: "Password doesn't match." });
+      return res.status(401).json({ error: 'Password does not match.' });
     }
 
     const { id, name } = user;
---
+++ src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+  secret: '2e8fbe297348e0796ef3313c5fc5d210',
+  expiresIn: '7d',
+};
